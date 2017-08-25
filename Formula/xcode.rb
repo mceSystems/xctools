a6e602d108f967f20483db99f2dcc5373afd4a5b
Pedro Piñera <pepibumur@gmail.com>
2017-08-25 17:56:45 +0200

    Fix formula

--- Formula/xcode.rb
+++ Formula/xcode.rb
@@ -5,13 +5,12 @@ class Xcode < Formula
     sha256 "c885d4349f32931560ac44ffa65a5209e48c875686ef1e3fec2a33d2d229cc6c"
     head "https://github.com/swift-xcode/xcode.git"
 
-    depends_on :xcode
+    depends_on :xcode => "9.0"
 
     def install
       xcode_path = "#{buildpath}/.build/release/xcode"
       ohai "Building xcode"
-      system("swift build -c release -Xlinker -rpath -Xlinker @executable_path -Xswiftc -static-stdlib")
-      frameworks.install yaml_lib_path
+      system("swift build -c release")
       bin.install xcode_path
     end
 
