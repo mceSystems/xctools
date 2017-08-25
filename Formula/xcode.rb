270485c42e67698490a0f6ccc3580d456dfeabc6
Pedro Piñera <pepibumur@gmail.com>
2017-08-25 17:43:39 +0200

    Update Homebrew formulat to point to 0.0.2

--- Formula/xcode.rb
+++ Formula/xcode.rb
@@ -1,9 +1,9 @@
 class Xcode < Formula
     desc "Handy command line tool for Xcode"
-    homepage "https://github.com/pepibumur/xcode"
-    url "https://github.com/pepibumur/xcode/archive/0.0.1.tar.gz"
-    sha256 "xxxx"
-    head "https://github.com/pepibumur/xcode.git"
+    homepage "https://github.com/swift-xcode/xcode"
+    url "https://github.com/swift-xcode/xcode/archive/0.0.2.tar.gz"
+    sha256 "c4099729b5a4f94eb0fcd8b114f0929f8c211f3f934b98684d906ef3d069490e"
+    head "https://github.com/swift-xcode/xcode.git"
 
     depends_on :xcode
 
