24f2420313d5b4551509df0baf2cd14ba0d3111d
Pedro Piñera <pepibumur@gmail.com>
2017-09-11 18:09:08 +0200

    [frameworks-embedding] Remove old tests

--- Tests/FrameworksTests/EmbedCommandTests.swift
+++ Tests/FrameworksTests/EmbedCommandTests.swift
@@ -3,26 +3,6 @@ import XCTest
 import PathKit
 import Frameworks
 
-// MARK: - SymbolsCopierTests
-
-final class SymbolsCopierTests: XCTestCase {
-    
-    func test_copyDsyms_copiesTheFilesFromAndToTheRightPaths() {
-        var from: Path!
-        var to: Path!
-        let subject = SymbolsCopier { (_from, _to) in
-            from = _from
-            to = _to
-        }
-        try? subject.copyDsyms(inputPath: "from/test.framework",
-                               destinationPath: "frameworks",
-                               keepingArchitectures: ["aa"])
-        XCTAssertEqual(from, "from/test.framework.dSYM")
-        XCTAssertEqual(to, "frameworks/test.framework.dSYM")
-    }
-    
-}
-
 // MARK: - EmbedErrorTests
 
 final class EmbedErrorTests: XCTestCase {
@@ -37,20 +17,8 @@ final class EmbedErrorTests: XCTestCase {
     
 }
 
-// MARK: - MockSymbolsCopyer
-
-final class MockSymbolsCopyer: SymbolsCopying {
+final class EmbedCommandTests: XCTestCase {
     
-    var copyDsymsCount: UInt = 0
-    var copyDsymsStub: Error? = nil
-    var copyDsymsSpy: (inputPath: Path, destinationPath: Path, keepingArchitectures: [String])?
     
-    func copyDsyms(inputPath: Path, destinationPath: Path, keepingArchitectures: [String]) throws {
-        copyDsymsCount += 1
-        copyDsymsSpy = (inputPath: inputPath, destinationPath: destinationPath, keepingArchitectures: keepingArchitectures)
-        if let copyDsymsStub = copyDsymsStub {
-            throw copyDsymsStub
-        }
-    }
     
 }
