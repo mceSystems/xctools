2a32f04b0ca89862591e74320327d6fff76fd500
dimitryvolkov <>
2019-04-14 12:30:09 +0300

    update formula to point to mceSystems repo address

--- Formula/xctools.rb
+++ Formula/xctools.rb
@@ -1,9 +1,9 @@
 class Xctools < Formula
   desc "Handy command line tool for Xcode"
-  homepage "https://github.com/xcodeswift/xctools"
-  url "https://github.com/xcodeswift/xctools/archive/0.3.0.tar.gz"
+  homepage "https://github.com/mceSystems/xctools"
+  url "https://github.com/mceSystems/xctools/archive/0.3.0.tar.gz"
   sha256 "3cfd3bc0c1161fd4c72b71bd0f707f8f4ab9e77acdc890983a3dc289a572630e"
-  head "https://github.com/xcodeswift/xctools.git"
+  head "https://github.com/mceSystems/xctools.git"
 
   depends_on :xcode => "9.0"
 
